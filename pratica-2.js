88725963bb9bf5bb119228ba405373f45200ff92
Thiagoramosmorato <111910722+Thiagoramosmorato@users.noreply.github.com>
2022-08-26 18:40:17 -0300

    Update pratica-2.js

---
+++ pratica-2.js
@@ -0,0 +1,99 @@
+const multiplicador = 2
+let i = 1
+
+while (i <= 10){
+const resultado = i * multiplicador
+const valorExibido = ` ${multiplicador} X $ {i} = ${resultado}`
+console . log (valorExibido)
+ resultado = (2)
+}
+
+const multiplicador = 2
+let i = 2
+
+while (i <= 10){
+const resultado = i * multiplicador
+const valorExibido = ` ${multiplicador} X $ {i} = ${resultado}`
+console . log (valorExibido)
+const resultado = (4)
+}
+
+const multiplicador = 2
+let i = 3
+
+while (i <= 10){
+const resultado = i * multiplicador
+const valorExibido = ` ${multiplicador} X $ {i} = ${resultado}`
+console . log (valorExibido)
+const resultado = (6)
+}
+
+const multiplicador = 2
+let i = 4
+
+while (i <= 10){
+const resultado = i * multiplicador
+const valorExibido = ` ${multiplicador} X $ {i} = ${resultado}`
+console . log (valorExibido)
+const resultado = (8)
+}
+
+const multiplicador = 2
+let i = 5
+
+while (i <= 10){
+const resultado = i * multiplicador
+const valorExibido = ` ${multiplicador} X $ {i} = ${resultado}`
+console . log (valorExibido)
+const resultado = (10)
+}
+
+const multiplicador = 2
+let i = 6
+
+while (i <= 10){
+const resultado = i * multiplicador
+const valorExibido = ` ${multiplicador} X $ {i} = ${resultado}`
+console . log (valorExibido)
+const resultado = (12)
+}
+
+const multiplicador = 2
+let i = 7
+
+while (i <= 10){
+const resultado = i * multiplicador
+const valorExibido = ` ${multiplicador} X $ {i} = ${resultado}`
+console . log (valorExibido)
+const resultado = (14)
+}
+
+const multiplicador = 2
+let i = 8
+
+while (i <= 10){
+const resultado = i * multiplicador
+const valorExibido = ` ${multiplicador} X $ {i} = ${resultado}`
+console . log (valorExibido)
+const resultado = (16)
+}
+
+const multiplicador = 2
+let i = 9
+
+while (i <= 10){
+const resultado = i * multiplicador
+const valorExibido = ` ${multiplicador} X $ {i} = ${resultado}`
+console . log (valorExibido)
+const resultado = (18)
+}
+
+const multiplicador = 2
+let i = 10
+
+while (i <= 10){
+const resultado = i * multiplicador
+const valorExibido = ` ${multiplicador} X $ {i} = ${resultado}`
+console . log (valorExibido)
+const resultado = (20)
+}
